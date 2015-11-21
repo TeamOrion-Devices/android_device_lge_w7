2ed1d52ca9ff6da33970904c493d2af7acb660b4
Quarx2k <agent00791@gmail.com>
2015-11-21 20:34:52 +0600

    Restore qc time daemon

--- device_w7.mk
+++ device_w7.mk
@@ -248,11 +248,6 @@ PRODUCT_PACKAGES += \
 
 NFCEE_ACCESS_PATH := device/lge/w7/prebuilt/etc/nfcee_access.xml
 
-# Sony timekeep
-PRODUCT_PACKAGES += \
-    timekeep \
-    TimeKeep
-
 # IO Scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
