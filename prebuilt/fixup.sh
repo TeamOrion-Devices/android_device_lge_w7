345c2e8124e845bf620142d6f904af470d189f30
Quarx2k <agent00791@gmail.com>
2015-11-22 19:00:02 +0600

    Fix selinux policy
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>
    
    Conflicts:
    	BoardConfig.mk
    	prebuilt/fixup.sh
    
    Change-Id: Ide7964302847c545ccc510b0544a27ce9c0bde36

--- prebuilt/fixup.sh
+++ prebuilt/fixup.sh
@@ -1,6 +1,7 @@
 #!/sbin/sh
 
-model=`getprop ro.product.model`
+model=`cat /proc/cmdline | sed 's/.*model.name=\([^ ]*\).*/\1/'`
+
 rm /system/usr/keylayout/Generic.kl
 
 if [ "$model" = "LG-D410" ] || [ "$model" = "LG-D410hn" ]; then
@@ -13,11 +14,11 @@ if [ "$model" = "LG-D410" ] || [ "$model" = "LG-D405" ] || [ "$model" = "LG-D415
 	rm /system/etc/permissions/android.hardware.nfc.xml
 	rm /system/etc/permissions/android.hardware.nfc.hce.xml
 	rm /system/lib/hw/nfc_nci.pn54x.default.so
-	rm -rf /system/app/NfcNci
+	rm -rf /system/app/NfcNci/
 fi
 
 if [ "$model" = "LG-D415" ]; then
 	rm /system/bin/fm_qsoc_patches
 	rm /system/etc/permissions/qcom.fmradio.xml
-	rm -rf /system/app/FM2
+	rm -rf /system/app/FM2/
 fi
